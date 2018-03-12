9a061820f43d92d844ec7d1332eec67bf3cb5549
Sean Oughton <seanoughton@gmail.com>
2018-03-12 21:14:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ class Student
 
   def save
     sql = <<-SQL
-    INSERT INTO students (name,grade)(?,?)
+    INSERT INTO students (name,grade)VALUES (?,?)
     SQL
     DB[:conn].execute(sql)
   end
